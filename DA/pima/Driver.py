34367b52a138db21cddcc74307b396710ebbc0d1
Saurabh Shinde <shindesaurabhpict@gmail.com>
2019-10-24 19:03:34 -0700

    fuck DA still not done

---
+++ DA/pima/Driver.py
@@ -0,0 +1,23 @@
+import csv
+import math
+import random
+
+
+def loadCsv(filename):
+    lines = csv.reader(open(filename, 'rt'))
+    dataset = list(lines)
+    dataset = dataset[1:]
+    for i in range(len(dataset)):
+        dataset[i] = [float(x) for x in dataset[i]]
+    return dataset
+
+def splitDataset(dataset, splitRatio):
+    trainSize = int(len(dataset) * splitRatio)
+    trainSet = []
+    copy = list(dataset)
+    while len(trainSet) < trainSize:
+        index = random.randrange(len(copy))
+        trainSet.append(copy.pop(index))
+    return [trainSet, copy]
+
+
